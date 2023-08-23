1706d17c6c4c253e16f4e382d2647ca9fbc4a29e
Badhon sarker <badhonsarker700@gmail.com>
2023-08-24 01:29:48 +0600

    Banner Section

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,97 +1,3 @@
-// import React from 'react';
-// import Banner from '../pages/Home/Home/Banner/Banner';
-// // import Banner from '../pages/Banner/Banner.jsx';
-// const Navbar = () => {
-//     return (
-//         <div className='h-screen' style={{ backgroundImage: "url('https://i.ibb.co/VggPtPN/hero-bg.png')" }}>
-
-//             <div className='mx-20'>
-//                 <div className="navbar  ">
-//                     <div className="navbar-start">
-//                         <div className="dropdown ">
-//                             <label tabIndex={0} className="btn btn-ghost lg:hidden text-white">
-//                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-//                             </label>
-//                             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-4 shadow bg-base-100 text-black rounded-box w-52">
-//                                 <li className="group">
-//                                     <a className="hover:text-[#e98c5d] font-bold">Home</a>
-//                                 </li>
-//                                 <li className="group">
-//                                     <a className="hover:text-[#e98c5d] font-bold">About</a>
-//                                 </li>
-//                                 <li className="group">
-//                                     <a className="hover:text-[#e98c5d] font-bold">Plan</a>
-//                                 </li>
-//                                 <li className="group relative ">
-//                                     <a className="hover:text-[#e98c5d] font-bold">Pages</a>
-//                                     <ul className="dropdown-content absolute hidden group-hover:block mt-6 py-4 text-white  bg-[#e98c5d] z-[5]  shadow-lg rounded-md w-52 ">
-//                                         <li className='hover:bg-[#e98c5d]  '><a className='hover:text-white'>Dashboard</a></li>
-//                                         <li className='hover:bg-[#e98c5d] '><a className='hover:text-white'>Blog Post</a></li>
-//                                         <li className='hover:bg-[#e98c5d] '><a className='hover:text-white'>Blog Details</a></li>
-//                                         <li className='hover:bg-[#e98c5d] '><a className='hover:text-white'>Create Account</a></li>
-//                                         <li className='hover:bg-[#e98c5d] '><a className='hover:text-white'>Privacy Policy</a></li>
-//                                         <li className='hover:bg-[#e98c5d] '><a className='hover:text-white'>Investor</a></li>
-//                                         <li className='hover:bg-[#e98c5d] '><a className='hover:text-white'>FAQ</a></li>
-//                                     </ul>
-//                                 </li>
-//                                 <li className="group">
-//                                     <a className="hover:text-[#e98c5d] font-bold">Blog</a>
-//                                 </li>
-//                                 <li className="group">
-//                                     <a className="hover:text-[#e98c5d] font-bold">Contact</a>
-//                                 </li>
-//                             </ul>
-//                         </div>
-//                         <a className="btn btn-ghost normal-case text-3xl font-bold text-white">InvestoGain</a>
-//                     </div>
-//                     <div className="navbar-center hidden lg:flex">
-//                         <ul className="menu menu-horizontal px-1 text-[18px] text-white">
-//                             <li className="group">
-//                                 <a className="hover:text-[#e98c5d] font-bold">Home</a>
-//                             </li>
-//                             <li className="group">
-//                                 <a className="hover:text-[#e98c5d] font-bold">About</a>
-//                             </li>
-//                             <li className="group">
-//                                 <a className="hover:text-[#e98c5d] font-bold">Plan</a>
-//                             </li>
-//                             <li className="group relative">
-//                                 <a className="hover:text-[#e98c5d] font-bold">Pages</a>
-//                                 <ul className="dropdown-content absolute hidden group-hover:block mt-9 py-4 bg-white text-black shadow-lg rounded-md w-44 pr-2 ">
-//                                     <li className='hover:bg-[#DE606B] rounded   '><a className='hover:text-white'>Dashboard</a></li>
-//                                     <li className='hover:bg-[#DE606B] rounded  '><a className='hover:text-white'>Blog Post</a></li>
-//                                     <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Blog Details</a></li>
-//                                     <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Create Account</a></li>
-//                                     <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Privacy Policy</a></li>
-//                                     <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Investor</a></li>
-//                                     <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>FAQ</a></li>
-//                                 </ul>
-//                             </li>
-//                             <li className="group">
-//                                 <a className="hover:text-[#e98c5d] font-bold">Blog</a>
-//                             </li>
-//                             <li className="group">
-//                                 <a className="hover:text-[#e98c5d] font-bold">Contact</a>
-//                             </li>
-//                         </ul>
-//                     </div>
-//                     <div className="navbar-end">
-//                         <button className="text-white uppercase font-bold border-2 hover:bg-[#DE606B] border-[#DE606B] px-8 py-2 rounded-2xl mr-2">SignUp</button>
-//                         <button style={{ backgroundImage: "linear-gradient(to right, #de5c70 0%, #e98c5d 51%, #de5c70 100%)" }} className="text-white uppercase font-bold px-8 py-2.5 rounded-2xl">Login</button>
-//                     </div>
-//                 </div>
-
-//             </div>
-//             <Banner></Banner>
-
-//         </div>
-//     );
-// };
-
-// export default Navbar;
-
-
-
 import React from 'react';
 import Banner from '../pages/Home/Home/Banner/Banner';
 
@@ -108,32 +14,32 @@ const Navbar = () => {
                             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-4 shadow bg-base-100 text-black rounded-box w-52">
                                 {/* ... Mobile menu items ... */}
                                 <li className="group">
-                                 <a className="hover:text-[#e98c5d] font-bold">Home</a>
-                             </li>
-                             <li className="group">
-                                 <a className="hover:text-[#e98c5d] font-bold">About</a>
-                             </li>
-                            <li className="group">
-                                 <a className="hover:text-[#e98c5d] font-bold">Plan</a>
-                             </li>
-                             <li className="group relative">
-                                 <a className="hover:text-[#e98c5d] font-bold">Pages</a>
-                                <ul className="dropdown-content absolute hidden group-hover:block mt-9 py-4 z-[5] bg-white text-black shadow-lg rounded-md w-44 pr-2 ">
-                                     <li className='hover:bg-[#DE606B] rounded   '><a className='hover:text-white'>Dashboard</a></li>
-                                     <li className='hover:bg-[#DE606B] rounded  '><a className='hover:text-white'>Blog Post</a></li>
-                                    <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Blog Details</a></li>
-                                    <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Create Account</a></li>
-                                    <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Privacy Policy</a></li>
-                                    <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Investor</a></li>
-                                    <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>FAQ</a></li>
-                                 </ul>
-                             </li>
-                             <li className="group">
-                                 <a className="hover:text-[#e98c5d] font-bold">Blog</a>
-                             </li>
-                             <li className="group">
-                                <a className="hover:text-[#e98c5d] font-bold">Contact</a>
-                            </li>
+                                    <a className="hover:text-[#e98c5d] font-bold">Home</a>
+                                </li>
+                                <li className="group">
+                                    <a className="hover:text-[#e98c5d] font-bold">About</a>
+                                </li>
+                                <li className="group">
+                                    <a className="hover:text-[#e98c5d] font-bold">Plan</a>
+                                </li>
+                                <li className="group relative">
+                                    <a className="hover:text-[#e98c5d] font-bold">Pages</a>
+                                    <ul className="dropdown-content absolute hidden group-hover:block mt-9 py-4 z-[5] bg-white text-black shadow-lg rounded-md w-44 pr-2 ">
+                                        <li className='hover:bg-[#DE606B] rounded   '><a className='hover:text-white'>Dashboard</a></li>
+                                        <li className='hover:bg-[#DE606B] rounded  '><a className='hover:text-white'>Blog Post</a></li>
+                                        <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Blog Details</a></li>
+                                        <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Create Account</a></li>
+                                        <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Privacy Policy</a></li>
+                                        <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Investor</a></li>
+                                        <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>FAQ</a></li>
+                                    </ul>
+                                </li>
+                                <li className="group">
+                                    <a className="hover:text-[#e98c5d] font-bold">Blog</a>
+                                </li>
+                                <li className="group">
+                                    <a className="hover:text-[#e98c5d] font-bold">Contact</a>
+                                </li>
                             </ul>
                         </div>
                         <a href="/" className="btn btn-ghost normal-case text-3xl font-bold text-white ">InvestoGain</a>
@@ -142,30 +48,30 @@ const Navbar = () => {
                         <ul className="menu menu-horizontal px-1 text-xl text-white">
                             {/* ... Desktop menu items ... */}
                             <li className="group">
-                                 <a className="hover:text-[#e98c5d] font-bold">Home</a>
-                             </li>
-                             <li className="group">
-                                 <a className="hover:text-[#e98c5d] font-bold">About</a>
-                             </li>
+                                <a className="hover:text-[#e98c5d] font-bold">Home</a>
+                            </li>
                             <li className="group">
-                                 <a className="hover:text-[#e98c5d] font-bold">Plan</a>
-                             </li>
-                             <li className="group relative">
-                                 <a className="hover:text-[#e98c5d] font-bold">Pages</a>
+                                <a className="hover:text-[#e98c5d] font-bold">About</a>
+                            </li>
+                            <li className="group">
+                                <a className="hover:text-[#e98c5d] font-bold">Plan</a>
+                            </li>
+                            <li className="group relative">
+                                <a className="hover:text-[#e98c5d] font-bold">Pages</a>
                                 <ul className="dropdown-content absolute hidden group-hover:block mt-9 py-4 bg-white text-black shadow-lg rounded-md w-44 pr-2 ">
-                                     <li className='hover:bg-[#DE606B] rounded   '><a className='hover:text-white'>Dashboard</a></li>
-                                     <li className='hover:bg-[#DE606B] rounded  '><a className='hover:text-white'>Blog Post</a></li>
+                                    <li className='hover:bg-[#DE606B] rounded   '><a className='hover:text-white'>Dashboard</a></li>
+                                    <li className='hover:bg-[#DE606B] rounded  '><a className='hover:text-white'>Blog Post</a></li>
                                     <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Blog Details</a></li>
                                     <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Create Account</a></li>
                                     <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Privacy Policy</a></li>
                                     <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>Investor</a></li>
                                     <li className='hover:bg-[#DE606B]  rounded '><a className='hover:text-white'>FAQ</a></li>
-                                 </ul>
-                             </li>
-                             <li className="group">
-                                 <a className="hover:text-[#e98c5d] font-bold">Blog</a>
-                             </li>
-                             <li className="group">
+                                </ul>
+                            </li>
+                            <li className="group">
+                                <a className="hover:text-[#e98c5d] font-bold">Blog</a>
+                            </li>
+                            <li className="group">
                                 <a className="hover:text-[#e98c5d] font-bold">Contact</a>
                             </li>
                         </ul>
--- src/components/pages/Home/Home/Banner/Banner.jsx
+++ src/components/pages/Home/Home/Banner/Banner.jsx
@@ -1,27 +1,27 @@
-// import React from 'react';
+import React from 'react';
 
-// const Banner = () => {
-//     return (
-//         <div className="grid grid-cols-1 lg:grid-cols-2 pt-10 lg:pt-36 pb-16 lg:pb-72 mx-5 lg:mx-20">
-//             {/* Left Section */}
-//             <div className="w-full text-white p-8 lg:leading-relaxed">
-//                 <h2 className="text-3xl lg:text-5xl font-bold mb-4 lg:mb-6 lg:leading-tight">
-//                     Hyip Investment & Get <span className='text-[#DE606B]'>You Profit</span>
-//                 </h2>
-//                 <p className="mb-4 lg:mb-6 text-base lg:text-xl">
-//                     Maecenas tempus tellus eget condimentum rhoncus sem quam semper libero.
-//                 </p>
-//                 <button className="bg-[#DE606B] text-white py-2 px-4 rounded-lg hover:bg-[#DE5C70] uppercase font-bold">
-//                     Get started
-//                 </button>
-//             </div>
+const Banner = () => {
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-2 pt-10 lg:pt-36 pb-16 lg:pb-72 mx-5 lg:mx-20">
+           
+            <div className="w-full text-white p-8 lg:leading-relaxed">
+                <h2 className="text-3xl lg:text-5xl font-bold mb-4 lg:mb-6 lg:leading-tight">
+                    Hyip Investment & Get <span className='text-[#DE606B]'>You Profit</span>
+                </h2>
+                <p className="mb-4 lg:mb-6 text-base lg:text-xl">
+                    Maecenas tempus tellus eget condimentum rhoncus sem quam semper libero.
+                </p>
+                <button className="bg-[#DE606B] text-white py-2 px-4 rounded-lg hover:bg-[#DE5C70] uppercase font-bold">
+                    Get started
+                </button>
+            </div>
 
-//             {/* Right Section */}
-//             <div className="w-full">
-//                 <img src="https://i.ibb.co/DbcR4y4/hero-img.png" alt="Image" className="w-full h-auto" />
-//             </div>
-//         </div>
-//     );
-// };
+            
+            <div className="w-full">
+                <img src="https://i.ibb.co/DbcR4y4/hero-img.png" alt="Image" className="w-full h-auto" />
+            </div>
+        </div>
+    );
+};
 
-// export default Banner;
+export default Banner;
