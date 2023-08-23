c5225f311bb103d886d06d6b942e2507adc84fbf
Badhon sarker <badhonsarker700@gmail.com>
2023-08-24 01:32:04 +0600

    InvestmentFeatured section added

--- src/components/pages/Home/InvestmentFeatured/InvestmentFeatured.jsx
+++ src/components/pages/Home/InvestmentFeatured/InvestmentFeatured.jsx
@@ -1,66 +1,58 @@
-// import React from 'react';
+import React from 'react';
 
-// const InvestmentFeatured = () => {
-//     return (
-//         <div className='lg:mx-20 px-4 pb-24'>
+const InvestmentFeatured = () => {
+    return (
+        <div className='lg:mx-20 px-4 pb-24'>
 
-//             <div className="text-center pb-16 px-4">
-//                 <h1
-//                     className='text-white font-bold text-4xl lg:text-6xl py-5 px-2'>
-//                     Why Choose Our Investment
-//                 </h1>
-//                 <p className='text-xl text-white'>Curabitur ullamcorper ultricies nisi Nam eget dui. Etiam rhoncus fdsf df vulputate.</p>
-//             </div>
-
-
-//             <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
-
-//                 <div className=''>
-
-//                     <div className="text-white mb-10">
-//                         <h1 className='text-2xl font-bold mb-2'>Quick Withdrawal</h1>
-//                         <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
-//                     </div>
-//                     <div className="text-white">
-//                         <h1 className='text-2xl font-bold mb-2'>Reliability</h1>
-//                         <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
-//                     </div>
-
-//                 </div>
+            <div className="text-center pb-16 px-4">
+                <h1
+                    className='text-white font-bold text-4xl lg:text-6xl py-5 px-2'>
+                    Why Choose Our Investment
+                </h1>
+                <p className='text-xl text-white'>Curabitur ullamcorper ultricies nisi Nam eget dui. Etiam rhoncus fdsf df vulputate.</p>
+            </div>
 
 
-//                 <div className=''>
-
-//                     <div className="text-white mb-10">
-//                         <h1 className='text-2xl font-bold mb-2'>Strong Protection</h1>
-//                         <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
-//                     </div>
-//                     <div className="text-white">
-//                         <h1 className='text-2xl font-bold mb-2'>Comodo SSL</h1>
-//                         <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
-//                     </div>
-
-//                 </div>
-
-//                 <div className=''>
-
-//                     <div className="text-white mb-10">
-//                         <h1 className='text-2xl font-bold mb-2'>Protected Website</h1>
-//                         <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
-//                     </div>
-//                     <div className="text-white">
-//                         <h1 className='text-2xl font-bold mb-2'>Registered Company</h1>
-//                         <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
-//                     </div>
-
-//                 </div>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+                <div>
+                    <div className="text-white mb-10">
+                        <h1 className='text-2xl font-bold mb-2'>Quick Withdrawal</h1>
+                        <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
+                    </div>
+                    <div className="text-white">
+                        <h1 className='text-2xl font-bold mb-2'>Reliability</h1>
+                        <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
+                    </div>
+                </div>
 
 
+                <div className=''>
 
-//             </div>
+                    <div className="text-white mb-10">
+                        <h1 className='text-2xl font-bold mb-2'>Strong Protection</h1>
+                        <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
+                    </div>
+                    <div className="text-white">
+                        <h1 className='text-2xl font-bold mb-2'>Comodo SSL</h1>
+                        <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
+                    </div>
 
-//         </div>
-//     );
-// };
+                </div>
 
-// export default InvestmentFeatured;
+                <div className=''>
+
+                    <div className="text-white mb-10">
+                        <h1 className='text-2xl font-bold mb-2'>Protected Website</h1>
+                        <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
+                    </div>
+                    <div className="text-white">
+                        <h1 className='text-2xl font-bold mb-2'>Registered Company</h1>
+                        <p>Maecenas tempus tellus eget condimentum rhoncus sem quam semper.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default InvestmentFeatured;
